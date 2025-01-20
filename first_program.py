10b6623ad7fc7c0dd0446703663502126783e89c
Victoria Okpala <ivyuduak19@gmail.com>
2025-01-20 16:26:49 +0000

    Add files via upload
    how the program works
    Print a greeting.
    Ask for a name.
    Collect the name entered by the user and store it in a variable called name.
    Ask the user what city they were born in.
    Collect the name of the city entered by the user and store it in a variable called city.

---
+++ first_program.py
@@ -0,0 +1,11 @@
+
+print("Hello! Welcome to this program.")
+
+name = input("What is your name? ")
+
+
+print("Hello, " + name + "! Nice to meet you!")
+
+city = input("What city were you born in? ")
+
+print(city + " is a nice city, I have heard of it! I would love to live there some day!")
